86942a0816de4cb4629b8a84bda7392c00891bab
ArifIlham0 <marifilham999@gmail.com>
2025-03-27 22:59:15 +0700

    feature: configure react native reanimated and gesture handler

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,6 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
 const config = {};
+const mergedConfig = mergeConfig(getDefaultConfig(__dirname), config);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = wrapWithReanimatedMetroConfig(mergedConfig);
